90630c2fd7f11c237818c26ad7dc150d37262053
unknown <ginbuilds@gmail.com>
2018-12-01 16:24:00 +0800

    y

---
+++ home.js
@@ -0,0 +1,31 @@
+var my_name; 
+var my_age;
+var my_birthday;
+
+my_name = "stackup";
+my_age = 40;
+my_age = "1/3/1984";
+
+
+console.log(my_name);   
+console.log(my_age);
+console.log(my_birthday);
+
+var my_address = "JTC LaunchPad";
+
+var my_test;
+
+console.log( typeof(my_test) );
+console.log( typeof(my_name) );
+console.log( typeof(my_name) );
+
+var firstName;
+var lastName;
+
+firstName = "John";
+lastName = "Doe";
+space = " ";
+
+var fullName = firstName + space + lastName;
+
+console.log(fullName);
